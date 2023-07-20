7acf1eee35d82f70e0e2adb9d9150805e4748963
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2023-07-20 14:06:08 +0200

    added some logs to the cron job

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ dotenv.config()
 
 import { checkEnvs } from './utils/handleCheckingEnvs'
 import { mapProducts } from './utils/handleMappingProducts'
+import { time } from 'console'
 
 async function main() {
   //TODO: Create a function for handling env loading (Check all env fields and see if they have a value. Also if their typing is correct)
@@ -12,13 +13,20 @@ async function main() {
     console.log(result.error)
   } else {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+    console.log('Running the pre Cron job' + new Date().getTime())
     await mapProducts()
+    console.log('Finished the initial run' + new Date().getTime())
 
+    let hour = '6'
+    let minute = '30'
+    console.log(`Starting the Cron Scheduler to run every day at ${hour}:${minute}`)
     var CronJob = require('cron').CronJob
     var job = new CronJob(
-      '* 30 6 * * *',
+      `* ${minute} ${hour} * * *`,
       async function () {
+        console.log('Running the Cron job' + new Date().getTime())
         await mapProducts()
+        console.log('Finished the Cron job' + new Date().getTime())
       },
       null,
       true,
